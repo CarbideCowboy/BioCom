fb0003b58048efd028a8d5f4077d41d72cb72794
Zach Thomas <zach.thomas@pioneerrx.net>
2020-06-02 13:14:22 -0500

    Rewired the foreground dispatch system to happen on the main page rather than scanprompt. This will act as the "master dispatch" from here on out.

--- app/src/main/java/com/hoker/biocom/pages/MainActivity.java
+++ app/src/main/java/com/hoker/biocom/pages/MainActivity.java
@@ -7,10 +7,14 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.graphics.Color;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,13 +23,21 @@ import android.view.WindowManager;
 
 import com.google.android.material.navigation.NavigationView;
 import com.hoker.biocom.R;
+import com.hoker.biocom.utilities.TagHandler;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity
 {
+    //navigation drawer and toolbar
     private DrawerLayout mDrawer;
     private Toolbar mToolbar;
     private NavigationView mNavigationView;
     private ActionBarDrawerToggle mDrawerToggle;
+    //intent filter and foreground dispatch
+    IntentFilter[] intentFiltersArray;
+    PendingIntent pendingIntent;
+    NfcAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -34,8 +46,66 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.layout_main_activity);
 
         setStatusBarColor();
-
         setUpNavigationDrawer();
+        nfcPrimer();
+    }
+
+    public void nfcPrimer()
+    {
+        //setup the physical nfc interface
+        adapter = NfcAdapter.getDefaultAdapter(this);
+
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        //set up NDEF_DISCOVERED filter for the foreground dispatch system
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try
+        {
+            ndef.addDataType("text/plain");
+        }
+        catch (IntentFilter.MalformedMimeTypeException e)
+        {
+            throw new RuntimeException("fail", e);
+        }
+
+        //create a filter array for the foreground dispatch system
+        intentFiltersArray = new IntentFilter[] {ndef, };
+
+        //checks to see if the launch intent is NDEF_DISCOVERED
+        handleActionNdefDiscovered(this.getIntent());
+    }
+
+    public void handleActionNdefDiscovered(Intent intent)
+    {
+        if(Objects.equals(intent.getAction(), NfcAdapter.ACTION_NDEF_DISCOVERED))
+        {
+            String ndefStringMessage = TagHandler.parseStringNdefPayload(intent);
+            Log.d("TEST MESSAGE", ndefStringMessage);
+            Intent ndefReadIntent = new Intent(this, NdefRead.class);
+            ndefReadIntent.putExtra("StringNDEF", ndefStringMessage);
+            startActivity(ndefReadIntent);
+        }
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        adapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        adapter.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, null);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent)
+    {
+        super.onNewIntent(intent);
+        handleActionNdefDiscovered(intent);
     }
 
     private void setUpNavigationDrawer()
